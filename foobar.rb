6230616faae34492c209f760e9d685e08152bccc
Ashley Hui <ashleynhui@gmail.com>
2016-09-21 23:24:33 -0700

    finished

--- foobar.rb
+++ foobar.rb
@@ -3,6 +3,14 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+    b = a
+    b.map! { |c| Integer(c)}
+    b.select! { |num| num.even?}
+    b.select! { |num| num < 10}
+    add_two = b.length*2
+    sum = 0
+    b.each { |c| sum+=c}
+    return add_two + sum
   end
 end
 
--- hw1.rb
+++ hw1.rb
@@ -1,20 +1,23 @@
 def squared_sum(a, b)
-  # Q1 CODE HERE
-  # blah
+    # Q1 CODE HERE
+    return (a + b)**2
+
 end
 
 def sort_array_plus_one(a)
-  # Q2 CODE HERE
-
+    # Q2 CODE HERE
+    return a.sort.map! { |b| b + 1}
 end
 
-def combine_name(first_name, last_name)
-  # Q3 CODE HERE
 
+
+def combine_name(first_name, last_name)
+    # Q3 CODE HERE
+    return first_name + ' ' + last_name
 end
 
 def blockin_time(a)
-  # DO NOT EDIT THIS CODE BELOW
-  require './foobar'
-  Foobar.baz a
+    # DO NOT EDIT THIS CODE BELOW
+    require './foobar'
+    Foobar.baz a
 end
